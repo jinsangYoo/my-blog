326a0af72c6eaa0669d3c5f08a7d68576b9fbdcc
jinsangYoo <jinsang@nhndata.com>
2021-06-02 16:59:15 +0900

    대문 문구 수정

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-header">Welcome to React</h1>
+        <h1 className="App-header">Hello, World!</h1>
         <p>진상's NAS.</p>
         <a
           className="App-link"
