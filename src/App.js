deb2888022a7b96907769c07a3f9643a7af53916
jinsangYoo <jinsang@nhndata.com>
2021-06-04 03:54:46 +0900

    todo 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-header">Hello, World!</h1>
-        <p>진상's NAS.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        { title: "Todo 1번째", id: 0 },
+        { title: "Todo 2번째", id: 1 },
+      ],
+      uniqueId: 1,
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.resetTodo = this.resetTodo.bind(this);
+  }
+
+  resetTodo() {
+    this.setState({
+      tasks: [],
+    });
+  }
+
+  addTodo(title) {
+    const { tasks, uniqueId } = this.state;
+
+    tasks.push({
+      title,
+      id: uniqueId,
+    });
+
+    this.setState({
+      tasks,
+      uniqueId: uniqueId + 1,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>TODO App</h1>
+        <button onClick={this.resetTodo}>리셋</button>
+        <TodoInput addTodo={this.addTodo} />
+        <TodoList tasks={this.state.tasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
