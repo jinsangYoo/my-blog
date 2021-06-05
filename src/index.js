54f4a2c4dbae57a9bac775a30fe7d60299c9703e
jinsangYoo <jinsang@nhndata.com>
2021-06-06 01:46:25 +0900

    스토어, index.js 만 사용 함

--- src/index.js
+++ src/index.js
@@ -1,15 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const initialState = {
+  task: "",
+  tasks: [],
+};
+
+const inputTask = (task) => ({
+  type: "INPUT_TASK",
+  payload: {
+    task,
+  },
+});
+
+function tasksReducer(state = initialState, action) {
+  switch (action.type) {
+    case "INPUT_TASK":
+      return {
+        ...state,
+        task: action.payload.task,
+      };
+    case "ADD_TASK":
+      return {
+        ...state,
+        tasks: state.tasks.concat([action.payload.task]),
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(tasksReducer);
+
+const addTask = (task) => ({
+  type: "ADD_TASK",
+  payload: {
+    task,
+  },
+});
+
+function TodoApp({ store }) {
+  console.log(JSON.stringify(store.getState()));
+  const { task, tasks } = store.getState();
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => store.dispatch(inputTask(e.target.value))}
+      />
+      <input
+        type="button"
+        value="add"
+        onClick={() => store.dispatch(addTask(task))}
+      />
+      <ul>
+        {tasks.map(function (item, i) {
+          return <li key={i}>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function renderApp(store) {
+  ReactDOM.render(<TodoApp store={store} />, document.getElementById("root"));
+}
+
+store.subscribe(() => renderApp(store));
+renderApp(store);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
