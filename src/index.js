f95b139c5ca9a8a0ed9d29ba54e98b277834329d
jinsangYoo <jinsang@nhndata.com>
2021-06-08 16:47:48 +0900

    기능별로 파일분할

--- src/index.js
+++ src/index.js
@@ -1,70 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import tasksReducer from "./reducers/tasks";
+import TodoApp from "./components/TodoApp";
 import { createStore } from "redux";
 
-const initialState = {
-  task: "",
-  tasks: [],
-};
-
-const inputTask = (task) => ({
-  type: "INPUT_TASK",
-  payload: {
-    task,
-  },
-});
-
-function tasksReducer(state = initialState, action) {
-  switch (action.type) {
-    case "INPUT_TASK":
-      return {
-        ...state,
-        task: action.payload.task,
-      };
-    case "ADD_TASK":
-      return {
-        ...state,
-        tasks: state.tasks.concat([action.payload.task]),
-      };
-    default:
-      return state;
-  }
-}
-
 const store = createStore(tasksReducer);
-
-const addTask = (task) => ({
-  type: "ADD_TASK",
-  payload: {
-    task,
-  },
-});
-
-function TodoApp({ store }) {
-  console.log(JSON.stringify(store.getState()));
-  const { task, tasks } = store.getState();
-  return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => store.dispatch(inputTask(e.target.value))}
-      />
-      <input
-        type="button"
-        value="add"
-        onClick={() => store.dispatch(addTask(task))}
-      />
-      <ul>
-        {tasks.map(function (item, i) {
-          return <li key={i}>{item}</li>;
-        })}
-      </ul>
-    </div>
-  );
-}
 
 function renderApp(store) {
   ReactDOM.render(<TodoApp store={store} />, document.getElementById("root"));
