f727d8c6c63eb38e844e7ce955a0b63046b94e40
jinsangYoo <jinsang@nhndata.com>
2021-06-09 18:16:56 +0900

    매터리얼 UI, 애니매이션 적용

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,16 +1,44 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import "./TodoApp.css";
 
-export default function TodoApp({ task, tasks, inputTask, addTask, redirectToError }) {
+export default function TodoApp({ task, tasks, inputTask, addTask }) {
   return (
     <div>
-      <input type="text" onChange={(e) => inputTask(e.target.value)} />
-      <input type="button" value="add" onClick={() => addTask(task)} />
-      <ul>
-        {tasks.map(function (item, i) {
-          return <li key={i}>{item}</li>;
-        })}
-      </ul>
-      <button onClick={() => redirectToError()}>오류 페이지</button>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Todo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div style={{ padding: "16px" }}>
+        <Input onChange={(e) => inputTask(e.target.value)} />
+        <Button variant="contained" color="secondary" onClick={() => addTask(task)}>
+          add
+        </Button>
+        <List>
+          <ReactCSSTransitionGroup transitionName="example" transitionEnterTimeout={2000}>
+            {tasks.map(function (item, i) {
+              return (
+                <ListItem key={i}>
+                  <ListItemText primary={`・${item}`} />
+                </ListItem>
+              );
+            })}
+          </ReactCSSTransitionGroup>
+        </List>
+      </div>
     </div>
   );
 }
